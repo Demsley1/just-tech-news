3fd6fb83ff4933d77237b85a113dc86b1b1ed62f
Denva <emsley3684@gmail.com>
2021-12-21 13:11:14 -0500

    push and post routes/methods

--- server.js
+++ server.js
@@ -11,6 +11,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // turn on conneciton to db and server
-sequelize.sync({ force: false }).then(() => {
+sequelize.sync({ force: true }).then(() => {
     app.listen(PORT, () => console.log('Now listening'));
 });
